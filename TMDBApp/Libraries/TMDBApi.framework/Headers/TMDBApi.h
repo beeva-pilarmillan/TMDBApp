d06d90a9c6f6a4f885fe4a2eada794ff1cafea7f
Pilar Millán <pilar.millan@bbva.com>
2017-01-12 18:58:29 +0100

    [TMDBApp] Initial commit

---
+++ TMDBApp/Libraries/TMDBApi.framework/Headers/TMDBApi.h
@@ -0,0 +1,17 @@
+//
+//  TMDBApi.h
+//  TMDBApi
+//
+//  Created by Pilar Millán on 8/1/17.
+//  Copyright © 2017 BEEVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDBApi.
+FOUNDATION_EXPORT double TMDBApiVersionNumber;
+
+//! Project version string for TMDBApi.
+FOUNDATION_EXPORT const unsigned char TMDBApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBApi/PublicHeader.h>
---
+++ TMDBApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TMDBApp
+//
+//  Created by Pilar Millán on 8/1/17.
+//  Copyright © 2017 BEEVA. All rights reserved.
+//
+
+import UIKit
+import TMDBApi
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
